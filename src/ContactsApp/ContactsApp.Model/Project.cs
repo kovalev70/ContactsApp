d823af2cec0ef2b36d09948f3def64c2f0c68bf2
kovalev70 <kovalev85291@gmail.com>
2023-04-27 00:43:00 +0700

    feature(Contact, Project): have been add classes Contact, Project and fields and methods of this classes.

---
+++ src/ContactsApp/ContactsApp.Model/Project.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Model
+{
+    /// <summary>
+    /// Класс проекта.
+    /// </summary>
+    public class Project
+    {
+        /// <summary>
+        /// Список контактов.
+        /// </summary>
+        private List<Contact> _contacts = new List<Contact>();
+        /// <summary>
+        /// Добаввляет контакт в список.
+        /// </summary>
+        /// <param name="contact"></param>
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+        /// <summary>
+        /// Удалаяет контакт из списка.
+        /// </summary>
+        /// <param name="contact"></param>
+        public void RemoveContact(Contact contact)
+        {
+            _contacts.Remove(contact);
+        }
+        /// <summary>
+        /// Возвращает список контактов.
+        /// </summary>
+        /// <returns></returns>
+        public List<Contact> GetAllContacts()
+        {
+            return _contacts;
+        }
+    }
+}
